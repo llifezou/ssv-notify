47e918af5597387dfc16ad6b05483952e772bef7
llifezou <llifezou@gmail.com>
2024-03-06 22:02:26 +0800

    update cmd

--- go.mod
+++ go.mod
@@ -3,32 +3,48 @@ module github.com/llifezou/ssv-notify
 go 1.20
 
 require (
-	github.com/gorilla/websocket v1.5.1
+	github.com/ethereum/go-ethereum v1.10.16
+	github.com/ipfs/go-log/v2 v2.5.1
 	github.com/spf13/cobra v1.8.0
 	github.com/spf13/viper v1.18.2
 	golang.org/x/xerrors v0.0.0-20231012003039-104605ab7028
 )
 
 require (
+	github.com/StackExchange/wmi v0.0.0-20180116203802-5d049714c4a6 // indirect
+	github.com/btcsuite/btcd v0.20.1-beta // indirect
+	github.com/deckarep/golang-set v1.8.0 // indirect
 	github.com/fsnotify/fsnotify v1.7.0 // indirect
+	github.com/go-ole/go-ole v1.2.1 // indirect
+	github.com/go-stack/stack v1.8.0 // indirect
+	github.com/google/uuid v1.4.0 // indirect
+	github.com/gorilla/websocket v1.5.1 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/magiconair/properties v1.8.7 // indirect
+	github.com/mattn/go-isatty v0.0.17 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/pelletier/go-toml/v2 v2.1.0 // indirect
+	github.com/rjeczalik/notify v0.9.1 // indirect
 	github.com/sagikazarmark/locafero v0.4.0 // indirect
 	github.com/sagikazarmark/slog-shim v0.1.0 // indirect
+	github.com/shirou/gopsutil v3.21.4-0.20210419000835-c7a38de76ee5+incompatible // indirect
 	github.com/sourcegraph/conc v0.3.0 // indirect
 	github.com/spf13/afero v1.11.0 // indirect
 	github.com/spf13/cast v1.6.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/subosito/gotenv v1.6.0 // indirect
+	github.com/tklauser/go-sysconf v0.3.5 // indirect
+	github.com/tklauser/numcpus v0.2.2 // indirect
 	go.uber.org/atomic v1.9.0 // indirect
 	go.uber.org/multierr v1.9.0 // indirect
+	go.uber.org/zap v1.21.0 // indirect
+	golang.org/x/crypto v0.16.0 // indirect
 	golang.org/x/exp v0.0.0-20230905200255-921286631fa9 // indirect
 	golang.org/x/net v0.19.0 // indirect
 	golang.org/x/sys v0.15.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
+	gopkg.in/natefinch/npipe.v2 v2.0.0-20160621034901-c1b8fa8bdcce // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -1,50 +1,116 @@
 package main
 
 import (
+	logging "github.com/ipfs/go-log/v2"
 	"github.com/llifezou/ssv-notify/config"
-	"github.com/llifezou/ssv-notify/ssv"
+	"github.com/llifezou/ssv-notify/notify"
+	"github.com/llifezou/ssv-notify/ssv/liquidation"
+	"github.com/llifezou/ssv-notify/ssv/operator"
 	"github.com/spf13/cobra"
-	"log"
 	"os"
 	"os/signal"
 	"syscall"
 )
 
-var configPath string
+var log = logging.Logger("main")
+
+var (
+	configPath string
+	dir        string
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "ssv-notify",
+	Short: "ssv-notify",
+	Long:  `ssv monitoring notifications.`,
+	PersistentPreRun: func(cmd *cobra.Command, args []string) {
+	},
+}
 
 func init() {
-	runCmd.PersistentFlags().StringVarP(&configPath, "config", "c", "./config/config.yaml", "Path to configuration file")
+	rootCmd.PersistentFlags().StringVarP(&configPath, "config", "c", "./config/config.yaml", "Path to configuration file")
+	scanClusterCmd.PersistentFlags().StringVarP(&dir, "dir", "d", "", "Save scan results")
 }
 
 func main() {
-	var rootCmd = &cobra.Command{
-		Use:   "ssv-notify",
-		Short: "ssv-notify",
-		Long:  `ssv operator monitoring notifications.`,
-		PersistentPreRun: func(cmd *cobra.Command, args []string) {
-		},
-	}
+	_ = logging.SetLogLevel("*", "INFO")
+	config.Init(configPath)
 
-	rootCmd.AddCommand(runCmd)
+	ssvToolsCmd.AddCommand(scanClusterCmd)
+	rootCmd.AddCommand(operatorMonitorCmd, liquidationMonitorCmd, ssvToolsCmd, AlarmTestCmd)
+
 	_ = rootCmd.Execute()
 }
 
-var runCmd = &cobra.Command{
-	Use:     "run",
-	Short:   "run monitor",
-	Example: "./ssv-notify run",
+var liquidationMonitorCmd = &cobra.Command{
+	Use:     "liquidation-monitor",
+	Short:   "liquidation monitor",
+	Example: "./ssv-notify liquidation-monitor",
 	Run: func(cmd *cobra.Command, args []string) {
-		log.Println("monitor service start")
-		config.Init(configPath)
+		log.Info("liquidation monitor service start")
 
 		var shutdown = make(chan struct{})
-		go ssv.StartMonitor(shutdown)
+		go liquidation.StartLiquidationMonitor(shutdown)
 
 		quit := make(chan os.Signal)
 		signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
 		<-quit
 		close(shutdown)
 
-		log.Println("monitor service shutting down")
+		log.Info("liquidation monitor service shutting down")
+	},
+}
+
+var operatorMonitorCmd = &cobra.Command{
+	Use:     "operator-monitor",
+	Short:   "operator monitor",
+	Example: "./ssv-notify operator-monitor",
+	Run: func(cmd *cobra.Command, args []string) {
+		log.Info("operator monitor service start")
+
+		var shutdown = make(chan struct{})
+		go operator.StartOperatorMonitor(shutdown)
+
+		quit := make(chan os.Signal)
+		signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+		<-quit
+		close(shutdown)
+
+		log.Info("operator monitor service shutting down")
+	},
+}
+
+var ssvToolsCmd = &cobra.Command{
+	Use:   "ssv-tools",
+	Short: "ssv tools",
+	Long:  `./ssv-notify ssv-tools -h`,
+	PersistentPreRun: func(cmd *cobra.Command, args []string) {
+	},
+}
+
+var scanClusterCmd = &cobra.Command{
+	Use:     "scan-cluster",
+	Short:   "Scan cluster and calculate operational runway",
+	Example: "./ssv-notify ssv-tools scan-cluster",
+	Run: func(cmd *cobra.Command, args []string) {
+		log.Info("Start scanning")
+		err := liquidation.ScanAllSSVCluster(dir)
+		if err != nil {
+			log.Warnw("ScanAllSSVCluster failed", "err", err)
+		}
+	},
+}
+
+var AlarmTestCmd = &cobra.Command{
+	Use:     "alarm-test",
+	Short:   "Test alarms can be used",
+	Example: "./ssv-notify alarm-test",
+	Run: func(cmd *cobra.Command, args []string) {
+		log.Info("Start alarm test")
+		notifyClient, err := notify.NewNotify()
+		if err != nil {
+			log.Warn(err)
+		}
+		notifyClient.Send("alarm test!")
 	},
 }
